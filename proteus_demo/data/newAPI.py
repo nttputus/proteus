f4b6951ab664677b5d24d4f467b275b78b60055e
Pierrick Koch <pierrick.koch@gmail.com>
2011-11-10 14:42:31 +0700

    [demo data] update scripts

--- proteus_demo/data/newAPI.py
+++ proteus_demo/data/newAPI.py
@@ -8,54 +8,64 @@ access `.blend` files. At the moment, robots, camera, and laser sensor are still
 read from `.blend` files due to their complexity (mesh or/and material).
 """
 
+#environment = 'land-1/trees' # outdoor
+environment = 'indoors-1/indoor-1' # indoor
+
+# in case you launche this script with a .blend file
+if len(bpy.data.objects) > 0:
+    environment = None
+
 # Append ATRV robot to the scene
 atrv = Robot('atrv')
+atrv.name = 'ATRV'
 
 # Append an actuator
 motion = MotionController()
-motion.translate(z=0.3)
+motion.name = 'Motion_Controller'
+motion.translate(z = 0.3)
 atrv.append(motion)
 
 # Append an odometry sensor
 odometry = Odometry()
-odometry.translate(x=-0.1, z=0.83)
+odometry.name = 'Odometry'
+odometry.translate(x = -0.1, z = 0.83)
 atrv.append(odometry)
-
-# Append a proximity sensor
-proximity = Proximity()
-proximity.translate(x=-0.2, z=0.83)
-atrv.append(proximity)
 
 # Append a Pose sensor (GPS + Gyroscope)
 pose = Pose()
-pose.translate(z=0.83)
+pose.name = 'Pose_sensor'
+pose.translate(x = -0.25, z = 0.83)
 atrv.append(pose)
 
 # Append a sick laser sensor
-sick = Sensor('sick')
-sick.translate(x=0.18,z=0.94)
+sick = Sick()
+sick.name = 'Sick'
+sick.translate(x = 0.18, z = 0.94)
 atrv.append(sick)
 sick.properties(resolution = 1)
+# sick.properties(scan_window = 270, resolution = .25)
+# sick.properties(scan_window = 180, resolution = 1)
 
 # Append a camera
-camera = Sensor('video_camera')
-camera.translate(x=0.3,z=1.1)
+camera = Camera()
+camera.name = 'CameraMain'
+camera.translate(x = 0.3, z = 0.96)
 atrv.append(camera)
-camera.properties(cam_width = 128, cam_height = 128)
+camera.properties(cam_width = 128, cam_height = 128, Vertical_Flip = True)
 
 # Append infrared sensors (left and right)
-infrared_r = Sensor('infrared')
-infrared_r.translate(x=0.6, y=-0.15,z=0.2)
-infrared_r.name = "InfraredR"
+infrared_r = Infrared()
+infrared_r.name = 'InfraredR'
+infrared_r.translate(x = 0.6, y = -0.15,z = 0.2)
 atrv.append(infrared_r)
-infrared_l = Sensor('infrared')
-infrared_l.translate(x=0.6, y=0.15,z=0.2)
-infrared_l.name = "InfraredL"
+infrared_l = Infrared()
+infrared_l.name = 'InfraredL'
+infrared_l.translate(x = 0.6, y = 0.15,z = 0.2)
 atrv.append(infrared_l)
 
 # Append a light actuator
 light = Light()
-light.translate(x=0.4,z=0.9)
+light.translate(x = 0.4, z = 0.9)
 atrv.append(light)
 
 # Append a battery sensor
@@ -65,7 +75,6 @@ atrv.append(battery)
 # Configure the middlewares
 motion.configure_mw('ros')
 odometry.configure_mw('ros')
-proximity.configure_mw('ros')
 pose.configure_mw('ros')
 sick.configure_mw('ros')
 camera.configure_mw('ros')
@@ -75,6 +84,6 @@ light.configure_mw('ros')
 battery.configure_mw('ros')
 
 # Select the environement
-env = Environment('indoors-1/indoor-1')
+env = Environment(environment)
 env.aim_camera([1.0470, 0, 0.7854])
 
